33a7275dbab7098e054a25a1bb98e4494fd59dc5
JLS <jonathan.sievers@gmail.com>
2022-10-24 12:32:20 -0400

    added ps4, tweaked ligo reader, added some radio slides

--- ligo_ffts/simple_read_ligo.py
+++ ligo_ffts/simple_read_ligo.py
@@ -7,9 +7,9 @@ plt.ion()
 def read_template(filename):
     dataFile=h5py.File(filename,'r')
     template=dataFile['template']
-    th=template[0]
-    tl=template[1]
-    return th,tl
+    tp=template[0]
+    tx=template[1]
+    return tp,tx
 def read_file(filename):
     dataFile=h5py.File(filename,'r')
     dqInfo = dataFile['quality']['simple']
@@ -40,23 +40,4 @@ strain,dt,utc=read_file(fname)
 
 #th,tl=read_template('GW150914_4_template.hdf5')
 template_name='GW150914_4_template.hdf5'
-th,tl=read_template(template_name)
-
-
-#spec,nu=measure_ps(strain,do_win=True,dt=dt,osamp=16)
-#strain_white=noise_filter(strain,numpy.sqrt(spec),nu,nu_max=1600.,taper=5000)
-
-#th_white=noise_filter(th,numpy.sqrt(spec),nu,nu_max=1600.,taper=5000)
-#tl_white=noise_filter(tl,numpy.sqrt(spec),nu,nu_max=1600.,taper=5000)
-
-
-#matched_filt_h=numpy.fft.irfft(numpy.fft.rfft(strain_white)*numpy.conj(numpy.fft.rfft(th_white)))
-#matched_filt_l=numpy.fft.irfft(numpy.fft.rfft(strain_white)*numpy.conj(numpy.fft.rfft(tl_white)))
-
-
-
-
-#copied from bash from class
-# strain2=np.append(strain,np.flipud(strain[1:-1]))
-# tobs=len(strain)*dt
-# k_true=np.arange(len(myft))*dnu
+tp,tx=read_template(template_name)
